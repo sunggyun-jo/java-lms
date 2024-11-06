365081813fe63039107b60fc4e7b9fdc569ae6af
sunggyun-jo <sunggyun.jo@gmail.com>
2024-11-06 22:56:02 +0900

    refactor: 답변 상태를 삭제 상태로 변경한다

--- src/test/java/nextstep/qna/domain/AnswerTest.java
+++ src/test/java/nextstep/qna/domain/AnswerTest.java
@@ -1,8 +1,20 @@
 package nextstep.qna.domain;
 
 import nextstep.users.domain.NsUserTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AnswerTest {
     public static final Answer A1 = new Answer(NsUserTest.JAVAJIGI, QuestionTest.Q1, "Answers Contents1");
     public static final Answer A2 = new Answer(NsUserTest.SANJIGI, QuestionTest.Q1, "Answers Contents2");
+
+    @Test
+    @DisplayName("답변 상태를 삭제 상태로 변경")
+    void deleteAnswer() {
+        assertThat(A1.isDeleted()).isFalse();
+        A1.delete();
+        assertThat(A1.isDeleted()).isTrue();
+    }
 }
